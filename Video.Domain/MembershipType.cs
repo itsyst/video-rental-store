a36626d8922e7a22b9e25fd33ae7b9135a24a7a5
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 22:16:40 +0100

    Add annotation to membershiptypes.

--- Video.Domain/MembershipType.cs
+++ Video.Domain/MembershipType.cs
@@ -1,9 +1,14 @@
-﻿namespace Video.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain;
 
 public class MembershipType
 {
+    [Key]
     public byte Id { get; set; }
     public short SignUpFee { get; set; }
+
+    [DataType(DataType.DateTime)]
     public byte Duration { get; set; }
     public byte DiscountRate { get; set; }
  }

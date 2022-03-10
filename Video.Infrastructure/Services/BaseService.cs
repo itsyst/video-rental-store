670ba87c47417733092c1bed3d88a53e4ec90e84
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:37:37 +0100

    Create a base repository service.

---
+++ Video.Infrastructure/Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Video.Application.Interfaces;
+using Video.Infrastructure.Persistence;
+
+namespace Video.Infrastructure.Services;
+
+public class BaseService<T> : IAsyncGenericRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+    internal readonly DbSet<T> _table;
+
+    public BaseService(ApplicationDbContext context)
+    {
+        _context = context;
+        _table = context.Set<T>();    
+    }
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _table.ToListAsync();
+    }
+}

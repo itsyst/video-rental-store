a9b59374d35e7a0974102ba889cfd662aa45171b
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:39:18 +0100

    Add a dbcontext.

---
+++ Video.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Video.Domain;
+
+namespace Video.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Movie>? Movies { get; set; }
+}

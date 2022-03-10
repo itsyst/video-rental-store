88a6dbe9e763a8dbc7a3fc826a3f38a5bbd7555e
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 17:06:34 +0100

    Create customer controller.

---
+++ Video.Web/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Video.Application.Interfaces;
+using Video.Domain;
+
+namespace Video.Web.Controllers;
+
+public class CustomersController : Controller
+{
+    private readonly ICustomerRepository _customer;
+    public CustomersController(ICustomerRepository customer)
+    {
+        _customer = customer;
+    }
+    public async Task<ActionResult> Index()
+    {
+        IEnumerable<Customer> customers = await _customer.GetAllAsync();
+        return View(customers);
+    }
+}

70b12f5dd5fb4dfd98d5d4b6eb98609fff63b3c0
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:39:00 +0100

    Create a movie repository service.

---
+++ Video.Infrastructure/Services/MovieService.cs
@@ -0,0 +1,12 @@
+﻿using Video.Application.Interfaces;
+using Video.Domain;
+using Video.Infrastructure.Persistence;
+
+namespace Video.Infrastructure.Services;
+
+public class MovieService : BaseService<Movie>, IMovieRepository
+{
+    public MovieService(ApplicationDbContext context) : base(context)
+    {
+    }
+}

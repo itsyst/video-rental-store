bb5d93986e966917358bcac7261a7c63b86562f4
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:43:30 +0100

    Create movie controller.

---
+++ Video.Web/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Video.Application.Interfaces;
+using Video.Domain;
+
+namespace Video.Web.Controllers;
+
+public class MoviesController : Controller
+{
+    private readonly IMovieRepository _movie;
+
+    public MoviesController(IMovieRepository movie)
+    {
+        _movie = movie;
+    }
+    public async Task<IActionResult> Index()
+    {
+        IEnumerable<Movie> movies = await _movie.GetAllAsync();
+        return View(movies);
+    }
+}

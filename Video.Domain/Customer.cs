d95242716fd119161d9630b0019fe94a7857d91f
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 22:22:34 +0100

    Apply limitation to customer name.

--- Video.Domain/Customer.cs
+++ Video.Domain/Customer.cs
@@ -9,10 +9,12 @@ public class Customer
 
     [Required]
     [Display(Name="First Name")]
+    [StringLength(50)]
     public string FirstName { get; set; } = string.Empty;
 
     [Required]
     [Display(Name = "Last Name")]
+    [StringLength(50)]
     public string LastName { get; set; } = string.Empty;
 
     [Display(Name = "Is Subscribed")]

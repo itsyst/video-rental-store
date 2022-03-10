b09f70604a27fc2051d78917466a2c3a4fdeca4d
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:42:47 +0100

    Create movie migration and update database.

---
+++ Video.Infrastructure/Migrations/20220309232358_AddMoviesTableToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Video.Infrastructure.Migrations;
+
+public partial class AddMoviesTableToDatabase : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Movies",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Movies", x => x.Id);
+            });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Movies");
+    }
+}

1a26763ceee64a46380fd6bcef1d381e1911dcb0
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 17:01:33 +0100

    Create customer model.

---
+++ Video.Domain/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain;
+
+public class Customer
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [Display(Name="First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+}

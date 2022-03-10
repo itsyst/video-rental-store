0d5642db68f681da8135fc3b3253320bed11a3d3
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 17:08:39 +0100

    Create error model and rewrite namespaces.

--- Video.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Video.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Video.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Video.Web.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

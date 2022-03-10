f8d9e822ecaa3b5fd5f37ffe8f842eed3fc219e2
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 21:28:41 +0100

    Create membershiptype model.

---
+++ Video.Domain/MembershipType.cs
@@ -0,0 +1,9 @@
+﻿namespace Video.Domain;
+
+public class MembershipType
+{
+    public byte Id { get; set; }
+    public short SignUpFee { get; set; }
+    public byte Duration { get; set; }
+    public byte DiscountRate { get; set; }
+ }

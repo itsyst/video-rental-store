848bc83829511407a62f438c9d9fd81c732e7f91
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 21:19:24 +0100

    Add isSubscribed and run migration.

---
+++ Video.Infrastructure/Migrations/20220310201220_AddIsSubscribedToCustomerModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Video.Infrastructure.Migrations
+{
+    public partial class AddIsSubscribedToCustomerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribed",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribed",
+                table: "Customers");
+        }
+    }
+}

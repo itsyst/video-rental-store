d6b6fc0efaa76eb9d16e70bf6065c4add543a880
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 13:42:12 +0100

    Create movie model.

---
+++ Video.Domain/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain;
+
+public class Movie
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "Poster")]
+    public string? ImageUrl { get; set; }
+}

8b828e4994163afa3b514b9bd58d52730acb8d54
Khaled El Hamzi <contact@elhamzi.me>
2022-03-10 17:06:11 +0100

    Create customer service.

---
+++ Video.Infrastructure/Services/Customer.cs
@@ -0,0 +1,12 @@
+﻿using Video.Application.Interfaces;
+using Video.Domain;
+using Video.Infrastructure.Persistence;
+
+namespace Video.Infrastructure.Services;
+
+public class CustomerService : BaseService<Customer>, ICustomerRepository
+{
+    public CustomerService(ApplicationDbContext context) : base(context)
+    {
+    }
+}

da2df07c4c2d75518d48005fb4721ae4ff261dac
Khaled El Hamzi <contact@elhamzi.me>
2022-03-11 19:18:33 +0100

    Create get all method with include state.

--- Video.Infrastructure/Services/CustomerService.cs
+++ Video.Infrastructure/Services/CustomerService.cs
@@ -1,12 +1,33 @@
-﻿using Video.Application.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Video.Application.Interfaces;
 using Video.Domain;
 using Video.Infrastructure.Persistence;
+
+#nullable disable
 
 namespace Video.Infrastructure.Services;
 
 public class CustomerService : BaseService<Customer>, ICustomerRepository
 {
+    public ApplicationDbContext _context;
     public CustomerService(ApplicationDbContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllCustomersAsync(params Expression<Func<Customer, object>>[] includeProperties)
+    {
+        IQueryable<Customer> query = _table;
+        if (includeProperties != null)
+        {
+            foreach (var includeProp in includeProperties)
+            {
+                query = query.Include(includeProp);
+            }
+
+        }
+
+        return await query.ToListAsync();
     }
 }

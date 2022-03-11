10d9142245924df0107f395bb0ea87489c389498
Khaled El Hamzi <contact@elhamzi.me>
2022-03-11 19:08:42 +0100

    Create migration to add foreignkey to customers table.

---
+++ Video.Infrastructure/Migrations/20220311162718_AddForeignKeyToCustomerTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Video.Infrastructure.Migrations
+{
+    public partial class AddForeignKeyToCustomerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_MembershipTypes_MembershipTypeId",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "MembershipTypeId",
+                table: "Customers",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_MembershipTypes_MembershipTypeId",
+                table: "Customers",
+                column: "MembershipTypeId",
+                principalTable: "MembershipTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_MembershipTypes_MembershipTypeId",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "MembershipTypeId",
+                table: "Customers",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_MembershipTypes_MembershipTypeId",
+                table: "Customers",
+                column: "MembershipTypeId",
+                principalTable: "MembershipTypes",
+                principalColumn: "Id");
+        }
+    }
+}

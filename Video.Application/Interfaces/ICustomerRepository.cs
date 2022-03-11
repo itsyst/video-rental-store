4f3c66d7e3585486bf7f9eb1f74bbf710497c284
Khaled El Hamzi <contact@elhamzi.me>
2022-03-11 19:16:15 +0100

    Introduce delegate to get all method in customer contract.

--- Video.Application/Interfaces/ICustomerRepository.cs
+++ Video.Application/Interfaces/ICustomerRepository.cs
@@ -1,7 +1,9 @@
-﻿using Video.Domain;
+﻿using System.Linq.Expressions;
+using Video.Domain;
 
 namespace Video.Application.Interfaces;
 
 public interface ICustomerRepository : IAsyncGenericRepository<Customer>
 {
+    Task<IEnumerable<Customer>> GetAllCustomersAsync(params Expression<Func<Customer, object>>[] includeProperties);
 }

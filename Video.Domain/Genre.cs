7adbadd25a9071d829b1aa7e6222af153620d0e2
Khaled El Hamzi <contact@elhamzi.me>
2022-03-12 17:30:36 +0100

    Add genre model.

---
+++ Video.Domain/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain
+{
+    public class Genre
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+    }
+}

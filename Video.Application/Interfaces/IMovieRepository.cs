f4e6d32ab683b89d0b2b13c9592835e604796773
Khaled El Hamzi <contact@elhamzi.me>
2022-03-12 22:42:57 +0100

    Add method with eager loading to movie contract.

--- Video.Application/Interfaces/IMovieRepository.cs
+++ Video.Application/Interfaces/IMovieRepository.cs
@@ -1,7 +1,9 @@
-﻿using Video.Domain;
+﻿using System.Linq.Expressions;
+using Video.Domain;
 
 namespace Video.Application.Interfaces;
 
 public interface IMovieRepository : IAsyncGenericRepository<Movie>
 {
+    Task<IEnumerable<Movie>> GetAllMoviesAsync(params Expression<Func<Movie, object>>[] includeProperties);
 }

7287ce1e33f041a0c0b702556acb830355c817b6
Khaled El Hamzi <contact@elhamzi.me>
2022-03-12 13:34:42 +0100

    Populate data and run migration.

---
+++ Video.Infrastructure/Migrations/20220311214327_AddBirthDateToCustomerTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Video.Infrastructure.Migrations
+{
+    public partial class AddBirthDateToCustomerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Customers");
+        }
+    }
+}

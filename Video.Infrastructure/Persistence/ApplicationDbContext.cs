a1550f15dc292e9e2422a31c66f13018ed1df575
Khaled El Hamzi <contact@elhamzi.me>
2022-03-12 20:17:44 +0100

    Enable sensitive dataLogging to see the conflicting key values.

--- Video.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Video.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Video.Domain;
 
 namespace Video.Infrastructure.Persistence;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext(
+        IConfiguration configuration, 
+        DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
-
+        _configuration = configuration;
     }
 
     public DbSet<Genre>? Genres { get; set; }
     public DbSet<Movie>? Movies { get; set; }
     public DbSet<MembershipType>? MembershipTypes { get; set; }
     public DbSet<Customer>? Customers { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:DefaultConnection"]);
+        optionsBuilder.EnableSensitiveDataLogging();
+    }
 }

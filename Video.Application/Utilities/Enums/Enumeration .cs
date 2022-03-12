8c3c43b3bf4f223b12ab1cf8d750d4738d613a16
Khaled El Hamzi <contact@elhamzi.me>
2022-03-12 13:29:50 +0100

    Create enumeration classes instead of using enum types.

---
+++ Video.Application/Utilities/Enums/Enumeration .cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace Video.Application.Utilities.Enums;
+
+#nullable disable
+public class Enumeration : IComparable
+{
+    public int Value { get; private set; }
+    public string Name { get; private set; }
+
+    protected Enumeration(ushort id, string name)
+    {
+        (Value, Name) = (id, name);
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+ 
+    public int CompareTo(object other)
+    {
+        return Value.CompareTo(((Enumeration)other).Value);
+    }
+
+    public static IEnumerable<T> GetAll<T>() where T : Enumeration
+    {
+        return typeof(T).GetFields(
+            BindingFlags.Public |
+            BindingFlags.Static |
+            BindingFlags.DeclaredOnly)
+            .Select(f => f.GetValue(null))
+            .Cast<T>();
+    }
+}

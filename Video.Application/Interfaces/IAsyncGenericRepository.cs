e98c0b512a002b5e4018dd9591d01f0b42698229
Khaled El Hamzi <contact@elhamzi.me>
2022-03-13 15:16:52 +0100

    Create add and update contracts.

--- Video.Application/Interfaces/IAsyncGenericRepository.cs
+++ Video.Application/Interfaces/IAsyncGenericRepository.cs
@@ -4,4 +4,6 @@ public interface IAsyncGenericRepository<T> where T : class
 {
     Task<T> GetByIdAsync(Guid id);
     Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
 }

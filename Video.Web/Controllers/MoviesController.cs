24bd9f6dddc74274e7520bffc91f10e534480869
Khaled El Hamzi <contact@elhamzi.me>
2022-03-14 16:45:56 +0100

    Create upsert action in movie controller.

--- Video.Web/Controllers/MoviesController.cs
+++ Video.Web/Controllers/MoviesController.cs
@@ -1,20 +1,116 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Video.Application.Interfaces;
 using Video.Domain;
+using Video.Web.ViewModels;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace Video.Web.Controllers;
 
 public class MoviesController : Controller
 {
     private readonly IMovieRepository _movie;
+    private readonly IGenreRepository _genre;
+    private readonly IWebHostEnvironment _hostEnvironment;
 
-    public MoviesController(IMovieRepository movie)
+    public MoviesController(
+        IMovieRepository movie,
+        IGenreRepository genre,
+        IWebHostEnvironment hostEnvironment)
     {
         _movie = movie;
+        _genre = genre;
+        _hostEnvironment = hostEnvironment;
     }
     public async Task<IActionResult> Index()
     {
         IEnumerable<Movie> movies = await _movie.GetAllMoviesAsync(includeProperties: m => m.Genre);
         return View(movies);
+    }
+
+    // GET: movies/Upsert/b86104b6-7205-4d5a-ab83-0eb534c0ae60
+    public async Task<IActionResult> Upsert(int id)
+    {
+        IEnumerable<Genre> genres = await _genre.GetAllAsync();
+        MovieViewModel model = new()
+        {
+            Movie = new(),
+            Genres = genres.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            })
+        };
+
+        if (id == 0)
+        {
+            return View(model);
+        }
+        else
+        {
+            // update customer
+            model.Movie = await _movie.GetByIdAsync(id);
+            return View(model);
+        }
+    }
+
+    // GET: Customers/Upsert/b86104b6-7205-4d5a-ab83-0eb534c0ae60
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(MovieViewModel model, IFormFile? file)
+    {
+         if (ModelState.IsValid)
+        {
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"uploads\posters");
+                var extension = Path.GetExtension(file.FileName).ToLower();
+
+                if (model.Movie.ImageUrl != null)
+                {
+                    var oldImagePath = Path.Combine(wwwRootPath, model.Movie.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    file.CopyTo(fileStreams);
+                }
+
+                model.Movie.ImageUrl = @"\uploads\posters\" + fileName + extension;
+
+
+                ResizeImage(file, Path.Combine(uploads, fileName + extension));
+            }
+
+            if (model.Movie.Id == 0)
+            {
+                model.Movie.CreatedDate = DateTime.Now;
+                await _movie.AddAsync(model.Movie);
+                TempData["Success"] = "Movie created successfully.";
+            }
+            else
+            {
+                await _movie.UpdateAsync(model.Movie);
+                TempData["Success"] = "Movie updated successfully.";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+        return View(model);
+    }
+
+    public void ResizeImage(IFormFile file, string path)
+    {
+        using var image = Image.Load(file.OpenReadStream());
+        image.Mutate(x => x.Resize(600, 900));
+        image.Save(path);
     }
 }

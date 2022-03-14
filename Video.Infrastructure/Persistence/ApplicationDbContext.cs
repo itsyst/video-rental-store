310e54686868b139495d500c9f495526abf478a7
Khaled El Hamzi <contact@elhamzi.me>
2022-03-14 16:39:42 +0100

    Reordre creating models .

--- Video.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Video.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -16,10 +16,10 @@ public class ApplicationDbContext : DbContext
         _configuration = configuration;
     }
 
-    public DbSet<Movie>? Movies { get; set; }
     public DbSet<Genre>? Genres { get; set; }
     public DbSet<MembershipType>? MembershipTypes { get; set; }
     public DbSet<Customer>? Customers { get; set; }
+    public DbSet<Movie>? Movies { get; set; }
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)

56498f9c11d516f6ab852efe2f937b4656216d9e
Khaled El Hamzi <contact@elhamzi.me>
2022-03-14 16:46:35 +0100

    Create movie view model.

---
+++ Video.Web/ViewModels/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Video.Domain;
+
+namespace Video.Web.ViewModels;
+
+public class MovieViewModel
+{
+    [ValidateNever]
+    public Movie? Movie { get; set; }
+    [ValidateNever]
+    public IEnumerable<SelectListItem>? Genres { get; set; }
+}

a0e326f48dc8be21d2592e585bef925483009d39
Khaled El Hamzi <contact@elhamzi.me>
2022-03-14 16:42:13 +0100

    Create contract and service customers.

---
+++ Video.Application/Interfaces/IGenreRepository.cs
@@ -0,0 +1,7 @@
+﻿using Video.Domain;
+
+namespace Video.Application.Interfaces;
+
+public interface IGenreRepository : IAsyncGenericRepository<Genre>
+{
+}
---
+++ Video.Infrastructure/Services/GenreService.cs
@@ -0,0 +1,16 @@
+﻿using Video.Application.Interfaces;
+using Video.Domain;
+using Video.Infrastructure.Persistence;
+
+#nullable disable
+
+namespace Video.Infrastructure.Services;
+
+public class GenreService : BaseService<Genre>, IGenreRepository
+{
+    private readonly ApplicationDbContext _context;
+    public GenreService(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}

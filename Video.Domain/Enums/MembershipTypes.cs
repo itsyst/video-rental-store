713ae4efd8ea7a20aa9caa2cc80fbc642b3d5b08
Khaled El Hamzi <contact@elhamzi.me>
2022-03-16 23:14:24 +0100

    Fix typo.

--- Video.Domain/Enums/MembershipTypes.cs
+++ Video.Domain/Enums/MembershipTypes.cs
@@ -3,7 +3,7 @@
 public class MembershipTypes : Enumeration
 {
     public static readonly MembershipTypes PAYASYOUGO = new(1, "Pay As You Go");
-    public static readonly MembershipTypes MOUNTHLY = new(2, "Mounthly");
+    public static readonly MembershipTypes MONTHLY = new(2, "Monthly");
     public static readonly MembershipTypes ANNUALLY = new(3, "Annually");
 
     protected MembershipTypes(ushort id, string name) : base(id, name)

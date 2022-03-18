91413ed22eb2be53efa7363eebce1e9785dcd632
Khaled El Hamzi <contact@elhamzi.me>
2022-03-18 23:20:11 +0100

    create profile mapper.

---
+++ Video.Application/Profiles/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Video.Application.Profiles.Dtos;
+using Video.Domain.Entities;
+
+namespace Video.Application.Profiles.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Customer, CustomerDto>().ReverseMap();
+        CreateMap<Movie, MovieDto>().ReverseMap();
+        CreateMap<Genre, GenreDto>().ReverseMap();
+        CreateMap<MembershipType, MembershipTypeDto>().ReverseMap();
+    }
+}

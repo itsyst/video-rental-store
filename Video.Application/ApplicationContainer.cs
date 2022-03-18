f6149039fde2a538b06fdf60f8fdc3a409a6b2ab
Khaled El Hamzi <contact@elhamzi.me>
2022-03-18 23:32:17 +0100

    Create application service container.

---
+++ Video.Application/ApplicationContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Video.Application;
+
+public static class ApplicationContainer
+{
+    //Services configuration
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}

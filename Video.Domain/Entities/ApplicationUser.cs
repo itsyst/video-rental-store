4211ed82382b2213c8290dd9e0111c45b32076ab
Khaled El Hamzi <contact@elhamzi.me>
2022-03-27 19:56:25 +0200

    Extend aspnet identity user.

---
+++ Video.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    [MaxLength(55)]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(55)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    [Required]
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
+
+    [Display(Name = "Phone Number")]
+    [DataType(DataType.PhoneNumber)]
+    [Required]
+    public string? PhoneNumber { get; set; }
+}

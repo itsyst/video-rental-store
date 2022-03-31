addb9f623018257af5e6f8bfe831bb88fb7b7f5c
Khaled El Hamzi <contact@elhamzi.me>
2022-03-31 11:44:34 +0200

    Rename role customer to guest.

--- Video.Domain/Utilities/Roles.cs
+++ Video.Domain/Utilities/Roles.cs
@@ -3,6 +3,6 @@
 public static class Roles
 {
     public const string Admin = "Admin";
-    public const string Customer = "Customer";
+    public const string Guest = "Guest";
 }
 
--- Video.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Video.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,7 +145,7 @@ public class RegisterModel : PageModel
             {
                 _logger.LogInformation("User created a new account with password.");
 
-                await _userManager.AddToRoleAsync(user, Roles.Customer);
+                await _userManager.AddToRoleAsync(user, Roles.Guest);
 
                 var userId = await _userManager.GetUserIdAsync(user);
                 var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);

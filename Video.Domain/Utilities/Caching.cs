81f3392748079e2e9fa64bc845bb203533e1e141
Khaled El Hamzi <contact@elhamzi.me>
2022-04-03 14:56:57 +0200

    Improve cache memory performance.

---
+++ Video.Domain/Utilities/Caching.cs
@@ -0,0 +1,7 @@
+﻿namespace Video.Domain.Utilities;
+
+public class Caching
+{
+    public const double Minutes = 2;
+    public const double Seconds = 120;
+}
--- Video.Web/Controllers/HomeController.cs
+++ Video.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 using Video.Application.Interfaces;
 using Video.Domain.Entities;
+using Video.Domain.Utilities;
 using Video.Web.ViewModels;
 
 namespace Video.Web.Controllers;
@@ -10,18 +12,32 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IMovieRepository _movie;
+    private IMemoryCache _cache;
 
-
-    public HomeController(ILogger<HomeController> logger, IMovieRepository movie)
+    public HomeController(ILogger<HomeController> logger, IMovieRepository movie, IMemoryCache memoryCache)
     {
         _logger = logger;
         _movie = movie;
+        _cache = memoryCache;
     }
 
     public async Task<IActionResult> Index()
     {
         IEnumerable<Movie> movies = await _movie.GetAllMoviesAsync(includeProperties: m => m.Genre);
-        return View(movies);
+
+        var cacheKey = "movies";
+
+        // Set cache options.
+        var memoryCacheEntryOptions = new MemoryCacheEntryOptions()
+        {
+            AbsoluteExpiration = DateTime.Now.AddSeconds(Caching.Seconds)
+
+        };
+
+        _cache.Set(cacheKey, movies, memoryCacheEntryOptions);
+        var cachedMovies = _cache.Get("movies");
+
+        return View(cachedMovies);
     }
 
     public IActionResult Privacy()

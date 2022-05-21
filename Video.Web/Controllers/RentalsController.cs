52ac8c648507f0dfcee3469f9d6ccd14387a7b27
Khaled El Hamzi <contact@elhamzi.me>
2022-05-21 23:58:48 +0200

    Create rental controller.

---
+++ Video.Web/Controllers/RentalsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Video.Application.Interfaces;
+using Video.Domain.Entities;
+using Video.Domain.Utilities;
+
+#nullable disable
+
+namespace Video.Web.Controllers;
+
+[Authorize(Roles = Roles.Admin)]
+public class RentalsController : Controller
+{
+    private readonly IRentalRepository _rental;
+    public RentalsController(IRentalRepository rental)
+    {
+        _rental = rental;
+    }
+
+    // GET: Customers/Upsert/5
+    public async Task<IActionResult> Create()
+    {
+        return await Task.FromResult(View());
+    }
+}

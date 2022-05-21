a72dad11eeb571f7b575c25d6137e066aafe3a95
Khaled El Hamzi <contact@elhamzi.me>
2022-05-21 23:53:17 +0200

    Create rental model.

---
+++ Video.Domain/Entities/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Video.Domain.Entities;
+
+public class Rental
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public Customer Customer { get; set; }
+
+    [Required]
+    public Movie Movie { get; set; }
+
+    public DateTime DateReturned { get; set; }
+    public DateTime DateRented { get; set; }
+}

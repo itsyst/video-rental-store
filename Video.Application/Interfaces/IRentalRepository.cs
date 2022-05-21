2d6c1b05ddc8f13972d7660ae17fbf0e770a6701
Khaled El Hamzi <contact@elhamzi.me>
2022-05-21 23:52:33 +0200

    create rental interface.

---
+++ Video.Application/Interfaces/IRentalRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using Video.Domain.Entities;
+
+namespace Video.Application.Interfaces;
+
+public interface IRentalRepository : IAsyncGenericRepository<Rental>
+{
+    Task<IEnumerable<Rental>> GetAllRentalsAsync(params Expression<Func<Rental, object>>[] includeProperties);
+    Task<Rental> GetRentalByIdAsync(int id);
+    Task<Rental> DeleteRentalByIdAsync(int id);
+}

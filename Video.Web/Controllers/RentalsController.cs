893a0091b6aee6c293e6ee5291b294d214024b13
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 02:23:41 +0200

    Add list and return actions.

--- Video.Web/Controllers/RentalsController.cs
+++ Video.Web/Controllers/RentalsController.cs
@@ -12,14 +12,47 @@ namespace Video.Web.Controllers;
 public class RentalsController : Controller
 {
     private readonly IRentalRepository _rental;
-    public RentalsController(IRentalRepository rental)
+    private readonly IMovieRepository _movie;
+    public RentalsController(
+        IRentalRepository rental,
+        IMovieRepository movie)
     {
         _rental = rental;
+        _movie = movie;
     }
 
-    // GET: Customers/Upsert/5
+    public async Task<ActionResult> Index()
+    {
+        IEnumerable<Rental> rentals = await _rental.GetAllRentalsAsync(filter:null, r=>r.Customer, r=>r.Movie);
+        return View(rentals);
+    }
+
+    // GET: Customers 
     public async Task<IActionResult> Create()
     {
         return await Task.FromResult(View());
     }
+
+
+    public async Task<IActionResult> Return(int id)
+    {
+        if (id == 0)
+            return NotFound();
+
+        var rentalInDb = await _rental.GetByIdAsync(id);
+ 
+        var rentals = await _rental.GetAllRentalsAsync(r=>r.Id == id, r => r.Customer, r => r.Movie);
+
+        foreach (var rental in rentals.ToList())
+        {
+            rental.Movie.InStock++;
+            await _movie.UpdateAsync(rental.Movie);
+            rental.DateReturned = DateTime.Now;
+
+            await _rental.UpdateAsync(rental);
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
 }

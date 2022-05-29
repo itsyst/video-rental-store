f6fbfdadff39493c67c378af5e9a5b9919520f7f
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 02:12:42 +0200

    Update rental contract - delegate

--- Video.Application/Interfaces/IRentalRepository.cs
+++ Video.Application/Interfaces/IRentalRepository.cs
@@ -5,7 +5,7 @@ namespace Video.Application.Interfaces;
 
 public interface IRentalRepository : IAsyncGenericRepository<Rental>
 {
-    Task<IEnumerable<Rental>> GetAllRentalsAsync(params Expression<Func<Rental, object>>[] includeProperties);
-    Task<Rental> GetRentalByIdAsync(int id);
+    Task<IEnumerable<Rental>> GetAllRentalsAsync(Expression<Func<Rental, bool>>? filter = null, params Expression<Func<Rental, object>>[] includeProperties);
+    Task<Rental> GetRentalByIdAsync(int id, params Expression<Func<Rental, object>>[] includeProperties);
     Task<Rental> DeleteRentalByIdAsync(int id);
 }

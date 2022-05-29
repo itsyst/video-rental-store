948ee935d6ea784b9c95a6fb7004a41f571f85b4
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 02:17:17 +0200

    Nullable property.

--- Video.Domain/Entities/Rental.cs
+++ Video.Domain/Entities/Rental.cs
@@ -6,12 +6,13 @@ public class Rental
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public Customer Customer { get; set; }
 
     [Required]
     public Movie Movie { get; set; }
 
-    public DateTime DateReturned { get; set; }
+    public DateTime? DateReturned { get; set; }
     public DateTime DateRented { get; set; }
 }

6ec4c3e3f4b704dfff5268daffb5492ca69c4775
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 02:16:47 +0200

    Modify rentalDto.

--- Video.Application/Profiles/Dtos/RentalDto.cs
+++ Video.Application/Profiles/Dtos/RentalDto.cs
@@ -5,9 +5,6 @@ namespace Video.Application.Profiles.Dtos;
 public class RentalDto
 {
     [Key]
-    public int Id { get; set; }
-    public CustomerDto? Customer { get; set; }
-    public MovieDto Movie { get; set; }
-    public DateTime DateReturned { get; set; }
-    public DateTime DateRented { get; set; }
+    public Guid CustomerId { get; set; }
+    public List<int> MovieIds { get; set; } = new List<int>();
 }

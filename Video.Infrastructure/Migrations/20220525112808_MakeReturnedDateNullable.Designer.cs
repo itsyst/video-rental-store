1aa5871fae766f5f21b32df682735e4c8ac78617
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 02:14:47 +0200

    Make return date property nullable.

--- Video.Infrastructure/Migrations/20220525112808_MakeReturnedDateNullable.Designer.cs
+++ Video.Infrastructure/Migrations/20220525112808_MakeReturnedDateNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Video.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using Video.Infrastructure.Persistence;
 namespace Video.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220525112808_MakeReturnedDateNullable")]
+    partial class MakeReturnedDateNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

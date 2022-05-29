999afd36d670e4e5ee8dd6b42b279f9a1db355d8
Khaled El Hamzi <contact@elhamzi.me>
2022-05-29 15:32:43 +0200

    Redirect 404 to home.

--- Video.Web/Program.cs
+++ Video.Web/Program.cs
@@ -64,6 +64,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.Use(async (context, next) =>
+    {
+        await next();
+        if (context.Response.StatusCode == 404)
+        {
+            context.Request.Path = "/Home";
+            await next();
+        }
+    });
 }
 else
 {
